95e6e82ab126fc22f2de05d866c1e1389b30c4b7
alejandro lüthi <alejandro.luthi@gmail.com>
2021-11-18 18:30:41 +0100

    added the controller and Service for the role

---
+++ src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
@@ -0,0 +1,40 @@
+package ch.zli.m223.punchclock.controller;
+
+import ch.zli.m223.punchclock.domain.Category;
+import ch.zli.m223.punchclock.domain.Project;
+import ch.zli.m223.punchclock.domain.Role;
+import ch.zli.m223.punchclock.service.CategoryService;
+import ch.zli.m223.punchclock.service.RoleService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/role")
+@Tag(name = "Role", description = "Handling of Role")
+public class RoleController {
+
+    @Inject
+    RoleService roleService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "List all Categories", description = "" )
+    public List<Role> list() {
+        return roleService.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Gets one Role", description = "Gets on Role by the ID")
+    public Role getSingleRole(@PathParam("id") Long id) {
+        return roleService.getSingleRole(id);
+    }
+}
