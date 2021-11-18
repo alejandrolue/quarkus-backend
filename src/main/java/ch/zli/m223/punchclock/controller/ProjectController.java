d1af7a69da01435377a252f2b7fd119b1530776b
alejandro lüthi <alejandro.luthi@gmail.com>
2021-11-18 14:11:21 +0100

    Created TeamsService.java with the functionalitys

--- src/main/java/ch/zli/m223/punchclock/controller/ProjectController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/ProjectController.java
@@ -1,6 +1,5 @@
 package ch.zli.m223.punchclock.controller;
 
-import ch.zli.m223.punchclock.domain.Entry;
 import ch.zli.m223.punchclock.domain.Project;
 import ch.zli.m223.punchclock.service.ProjectService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
---
+++ src/main/java/ch/zli/m223/punchclock/service/TeamsService.java
@@ -0,0 +1,37 @@
+package ch.zli.m223.punchclock.service;
+
+import ch.zli.m223.punchclock.domain.Project;
+import ch.zli.m223.punchclock.domain.Teams;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class TeamsService {
+    @Inject
+    private EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
+    public List<Teams> findAll() {
+        var query = entityManager.createQuery("From teams");
+        return query.getResultList();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Teams teamsToDelete = getSingleTeams(id);
+        entityManager.remove(teamsToDelete);
+    }
+
+    public Teams getSingleTeams(Long id) {
+        return entityManager.find(Teams.class, id);
+    }
+
+    @Transactional
+    public Teams createTeam(Teams team) {
+        entityManager.persist(team);
+        return team;
+    }
+}
+
