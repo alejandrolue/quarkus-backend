83836584831f55eb4777e29753cd9044e5621dbc
alejandro lüthi <alejandro.luthi@gmail.com>
2021-11-22 09:07:32 +0100

    sql import bearbeitet

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,4 +3,9 @@ INSERT INTO CATEGORY(title) VALUES ('Planung');
 INSERT INTO CATEGORY(title) VALUES ('Implementierung');
 INSERT INTO CATEGORY(title) VALUES ('Testen');
 
+INSERT INTO ROLE(roleName) VALUES ('Admin');
+INSERT INTO ROLE(roleName) VALUES ('Team-Leiter');
+INSERT INTO ROLE(roleName) VALUES ('Mitarbeiter');
+
 INSERT INTO USER (username, password) VALUES ('zli', 'secret');
+INSERT INTO USER (username, password) VALUES ('test', 'test');
